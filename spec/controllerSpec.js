6af6e4f0259140c248f7bd37ccf787192559bf01
Esam Al-Dabagh <esam.aldabagh@gmail.com>
2018-09-22 13:00:42 +0100

    Controller instantiates with an instance of AllPeepsView

--- spec/controllerSpec.js
+++ spec/controllerSpec.js
@@ -9,4 +9,8 @@ describe("Controller", function() {
   it("instantiates with an instance of the Peeps model", function() {
     expect(controller._peeps).toBeDefined();
   });
+  
+  it("instantiates with an instance of AllPeepsView", function() {
+    expect(controller._allPeepsView).toBeDefined();
+  });
 })
--- src/controller/Controller.js
+++ src/controller/Controller.js
@@ -1,8 +1,11 @@
 (function(exports) {
 
-  function Controller(peeps = new Peeps()) {
+  function Controller(peeps = new Peeps(), allPeepsView = AllPeepsView) {
     this._peeps = peeps;
+    this._allPeepsView = new allPeepsView(this._peeps);
   }
+
+  
 
   exports.Controller = Controller;
 })(this)
