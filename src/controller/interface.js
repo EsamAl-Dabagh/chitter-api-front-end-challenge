3d6615ca344721509df1f598807b1573501ddc41
Esam Al-Dabagh <esam.aldabagh@gmail.com>
2018-09-22 19:28:05 +0100

    add function in Interface to listen for clicks on register button

--- src/controller/interface.js
+++ src/controller/interface.js
@@ -2,4 +2,13 @@ document.addEventListener("DOMContentLoaded", function () {
   var controller = new Controller();
 
   controller.fetchAllPeeps();
+
+  (function reigsterButton() {
+    document.getElementById("registerBtn")
+      .addEventListener("click", function(clickEvent) {
+        clickEvent.preventDefault();
+        controller.createRegisterView();
+      })
+  })();
+
 })
